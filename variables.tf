4ba4f79961754df6bd25912cc1a6265f7066b777
Dragos Crisan <dragrom@gmail.com>
2018-05-19 10:31:05 +0300

    test

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable access_key {
     description = "Access key for aws user"
-    default = "AKIAIGUGZXTRMTY7KPBA"
+    default = ""
 }
 
 variable secret_key {
-    default = "XvLHsZlf8xwBNcvh1vFr8jkEZrCpzC2Coc/Ao7BI"
+    default = ""
 }
